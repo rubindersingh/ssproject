3e995cb032361ebb7397957892054c179b51d2ca
Rubinder Singh <rssandhu2011@gmail.com>
2017-04-29 17:05:45 -0700

    Backup

---
+++ Configuration.py
@@ -0,0 +1,54 @@
+import xml.etree.ElementTree as ET
+class Configuration:
+
+    def __init__(self, fileName, reload=-1):
+        tree = ET.parse(fileName)
+        self.configuration = tree.getroot()
+        self.services = {}
+        for service in self.configuration.find("services"):
+            self.services[int(service.find("external_port").text)] = Service(service)
+
+    def getService(self, port):
+        return self.services[port]
+
+class Service:
+    def __init__(self, service):
+        self.internal_port = int(service.find("internal_port").text)
+        self.external_port = int(service.find("external_port").text)
+        self.start_cmd = service.find("start_cmd").text
+        self.expected_args = {}
+        args = service.find("expected_args")
+        self.max_args = 0
+        self.min_args = 0
+        self.required_args = {}
+        self.optional_args = {}
+        i = 0
+        for arg in args:
+            self.max_args += 1
+            if arg.attrib.get('required', 0) == "1":
+                self.min_args += 1
+                if arg.attrib.get('name', None) is not None:
+                    self.expected_args[arg.attrib['name']] = arg
+                    self.required_args[arg.attrib['name']] = arg
+                elif arg.attrib.get('position', None) is not None:
+                    self.expected_args[int(arg.attrib['position'])] = arg
+                    self.required_args[int(arg.attrib['position'])] = arg
+                else:
+                    self.expected_args[i] = arg
+                    self.required_args[i] = arg
+            else:
+                if arg.attrib.get('name', None) is not None:
+                    self.expected_args[arg.attrib['name']] = arg
+                    self.optional_args[arg.attrib['name']] = arg
+                elif arg.attrib.get('position', None) is not None:
+                    self.expected_args[int(arg.attrib['position'])] = arg
+                    self.optional_args[int(arg.attrib['position'])] = arg
+                else:
+                    self.expected_args[i] = arg
+                    self.optional_args[i] = arg
+            i = i+1
+
+        filterList = service.find("filters")
+        self.filters = {}
+        for filter in filterList:
+            self.filters[filter.attrib['name']] = filter
---
+++ Firewall.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+from socket import *
+from subprocess import *
+import sys
+import os
+import time
+import threading as th
+import logging as log
+import Configuration as conf
+
+class Firewall:
+    port_map={}
+    #Initializing the necessary variables from CONFIG file
+    def __init__(self,configFilePath,logFilePath):
+        #Preparing the Logger for the Messages
+        self.logger = log.getLogger('Clippers')
+        self.logger.setLevel(log.DEBUG)
+
+        #File handler which creates a Fresh File everytime it is run
+        self.handler = log.FileHandler(logFilePath,mode='a')
+        self.handler.setLevel(log.DEBUG)
+
+        #Formatter to specify the format of the LOG messages
+        self.formatter = log.Formatter('%(levelname)s %(message)s')
+        self.handler.setFormatter(self.formatter)
+        self.logger.addHandler(self.handler)
+
+        #Extracting the CONFIG parameters
+        self.configuration = conf.Configuration(configFilePath)
+        self.logger.info('Loaded the Config File')
+        #self.logger.info(self.port_map + self.buffer + self.command+ self.path +'\n')
+
+    def map_services(self):
+        self.fork_process=[]
+
+        #Mapping the existing services to the Mapped ports
+        for port in self.configuration.services:
+            service = self.configuration.getService(port)
+            internal_port = service.internal_port
+            external_port = service.external_port
+            self.port_map[external_port] = internal_port
+
+            #print 'Forking for port ',port
+            PID_forked=os.fork()
+            if (PID_forked==0):
+                try:
+                    service_up=Popen(service.start_cmd,stdout=PIPE,stdin=None,close_fds=True, shell=True)
+                    print service_up
+                    #print 'Mapped the service at port %d to %d\n' % (port,mapped_port)
+                    self.logger.info('Mapped the service at port %d to %d\n' % (external_port,internal_port))
+
+                    #Killing the Existing Service running on PORT
+                    self.kill_service(external_port)
+                    time.sleep(2)
+
+                    #Starting the PROXY on the original port
+                    self.create_proxy(external_port)
+
+                    #Kill the child and preventing it from entering the loop
+                    sys.exit(0)
+                except Exception as exc:
+                    handle_Exception(exc)
+            else:
+                print 'The child forked is ',PID_forked
+                self.fork_process.append(PID_forked)
+
+    def kill_service(self,port):
+        #The PID parameter is the 2nd argument in the output of lsof
+        #Killing the Running Service, 9 stands for signal.SIGKILL
+        out=Popen(["lsof","-P","-i"],stdout=PIPE)
+        details=Popen(["grep",str(port)],stdin=out.stdout,stdout=PIPE)
+        proc_details=details.stdout.read()
+
+        if len(proc_details)!=0:
+            current_PID=proc_details.split()[1]
+            self.logger.info('PID of Original Service running at port '+str(port)+' is '+str(current_PID))
+            os.kill(int(current_PID),9)
+
+            #print 'Killed the Service running at port '+str(port)
+            self.logger.info('Killed the Service running at port '+str(port))
+        else:
+            print 'Unable to find the service on this port'
+
+    #Listening on the original port to listen to the incoming connections
+    def create_proxy(self,port):
+        try:
+            self.buffer = 65536
+            proxySocket = socket(AF_INET, SOCK_STREAM)
+            proxySocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+            proxySocket.bind(('',int(port)))
+            proxySocket.listen(5)
+
+            #print 'Proxy created and running on port', port
+            self.logger.info('Proxy CREATED and Running on PORT '+str(port))
+            while True:
+                request_sock,(clhost, clport)=proxySocket.accept()
+                data      = request_sock.recv(self.buffer)
+                print "Data at Proxy ", data
+                attributes= data.split()[1:]
+                param     = ''.join(str(word) for word in attributes)
+                #print 'Message Received at PROXY Port: ',param
+                self.logger.info('REQUEST at PROXY Port:' + str(port) + ' -> ' + str(param))
+                map_port=self.port_map[port]
+                response=self.proxy_client(data,map_port)
+                print "Response at Proxy ", response
+                self.logger.info('RESPONSE from MAIN Port:' + str(map_port) + ' -> ' + str(response))
+                request_sock.send(response)
+                request_sock.close()
+        except Exception as exc:
+            handle_Exception(exc)
+
+    def proxy_client(self,data,mapped_port):
+        try:
+            prxy_chld_sock = socket(AF_INET, SOCK_STREAM)
+            prxy_chld_sock.connect(('',int(mapped_port)))
+
+            prxy_chld_sock.send(data)
+            response=prxy_chld_sock.recv(self.buffer)
+
+            prxy_chld_sock.close()
+            return response
+        except Exception as exc:
+            handle_Exception(exc)
+
+    def alive_threads(self):
+        print 'The Active Threads are:'
+        for thread in th.enumerate():
+            print thread.getName()
+        return th.active_count()
+
+def handle_Exception(ex):
+    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+    message = template.format(type(ex).__name__, ex.args)
+    print message
+
+firewall=Firewall('config.xml','requests.log')
+firewall.map_services()
