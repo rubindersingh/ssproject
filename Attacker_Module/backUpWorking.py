047c5dc35537f91f2f14c206b2408e7e6b95cbe7
TejiKang <tkang8@asu.edu>
2017-04-30 18:08:14 -0700

    targets error resolved

--- Attacker_Module/backUpWorking.py
+++ Attacker_Module/backUpWorking.py
@@ -97,17 +97,17 @@ def replaceServiceVar(string,teamNum):
             
             #print var,var=="$_team;"
                 
-            if(var=="$_team;"):
+            """if(var=="$_team;"):
                 print var
                 #print serviceList[serviceNum][string[sindex+2:eindex]]
                 #val = str(serviceList[teamNum][string[sindex+2:eindex]])
-                val = str("team1")
+                val = str("team")+str(teamNum)
                 string = string.replace(var,val)
-            else:
-                print serviceList[int(serviceNum)][string[sindex+2:eindex]]
-                #val = str(serviceList[int(serviceNum)][string[sindex+2:eindex]])
-                val=t.get_targets(int(serviceList[int(serviceNum)][string[sindex+2:eindex]]))
-                string = string.replace(var,val)
+            else:"""
+            print targets[teamNum][string[sindex+2:eindex]]
+            val = str(targets[teamNum][string[sindex+2:eindex]])
+            #val=t.get_targets(int(serviceList[int(serviceNum)][string[sindex+2:eindex]]))
+            string = string.replace(var,val)
         else:
             nonServiceVarFound=1
 
@@ -227,12 +227,18 @@ def multiProcessAttacks(teamNum):
 
         
 def getServiceList():
-    global serviceList,t
+    global serviceList,t,serviceNum,targets
     
     flagsList = []
     threading.Timer(tick, getServiceList).start()
     
     serviceList=t.get_service_list()
+    targets = t.get_targets(int(serviceList[int(serviceNum)]))
+    
+   
+    #targets = {u'targets': [{u'flag_id': u'2134275124', u'team_name': u'Sheep', u'hostname': u'team24', u'port': 20001}, {u'flag_id': u'4103469502', u'team_name': u'WeLoveAdam', u'hostname': u'team20', u'port': 20001}, {u'flag_id': u'3447105310', u'team_name': u't34m_ro6u3', u'hostname': u'team21', u'port': 20001}, {u'flag_id': u'1324813728', u'team_name': u'Ping of Death', u'hostname': u'team22', u'port': 20001}, {u'flag_id': u'1705253942', u'team_name': u'Kitten Mittens', u'hostname': u'team23', u'port': 20001}, {u'flag_id': u'3641246892', u'team_name': u'PermissionDenied', u'hostname': u'team1', u'port': 20001}, {u'flag_id': u'4100535514', u'team_name': u'Very Unfair', u'hostname': u'team3', u'port': 20001}, {u'flag_id': u'1270352691', u'team_name': u'Code Yo Loco', u'hostname': u'team2', u'port': 20001}, {u'flag_id': u'1985929341', u'team_name': u'BlackBox', u'hostname': u'team5', u'port': 20001}, {u'flag_id': u'1716698999', u'team_name': u'404 : Team name not found', u'hostname': u'team4', u'port': 20001}, {u'flag_id': u'775004714', u'team_name': u'I Am Root', u'hostname': u'team7', u'port': 20001}, {u'flag_id': u'547913662', u'team_name': u'slimyfish', u'hostname': u'team6', u'port': 20001}, {u'flag_id': u'1442375046', u'team_name': u'P00P-Gormint', u'hostname': u'team9', u'port': 20001}, {u'flag_id': u'2019393330', u'team_name': u'Project0', u'hostname': u'team11', u'port': 20001}, {u'flag_id': u'2983061410', u'team_name': u'7Wonders', u'hostname': u'team10', u'port': 20001}, {u'flag_id': u'2162259043', u'team_name': u'buffercool', u'hostname': u'team13', u'port': 20001}, {u'flag_id': u'984302694', u'team_name': u'JEDI FORCE', u'hostname': u'team12', u'port': 20001}, {u'flag_id': u'1373744145', u'team_name': u'Undercover NOPeratives', u'hostname': u'team15', u'port': 20001}, {u'flag_id': u'4079514683', u'team_name': u'Toc_To_U', u'hostname': u'team14', u'port': 20001}, {u'flag_id': u'1561766352', u'team_name': u'0CHN', u'hostname': u'team17', u'port': 20001}, {u'flag_id': u'3927925538', u'team_name': u'SCARF-LV', u'hostname': u'team16', u'port': 20001}, {u'flag_id': u'515112532', u'team_name': u'Blitzkrieg', u'hostname': u'team19', u'port': 20001}, {u'flag_id': u'546971277', u'team_name': u'blckpwn', u'hostname': u'team18', u'port': 20001}]}
+    
+    targets=targets['targets']
     
     #serviceList=[{u'flag_id_description': u'Flags are identified by the note name.', u'description': u'Password-protected note storage service in C.', u'service_name': u'sample_c', u'team_id': 0, u'state': u'enabled', u'upload_id': 1, u'authors': u'UCSB', u'service_id': 10001, u'port': 10001,u'host':'localhost'}, {u'flag_id_description': u'Flags are identified by the note name.', u'description': u'Password-protected note storage service in Python.', u'service_name': u'sample_py', u'team_id': 0, u'state': u'enabled', u'upload_id': 2, u'authors': u'The iCTF team 2016', u'service_id': 10002, u'port': 10002,u'host':'localhost'}, {u'flag_id_description': u'Flags are identified by the note name.', u'description': u'Password-protected note storage service for the web.', u'service_name': u'sample_web', u'team_id': 0, u'state': u'enabled', u'upload_id': 3, u'authors': u'UCSB', u'service_id': 10003, u'port': 10003,u'host':'localhost'}]
         
