9e023f78bc914ec0ac20ed515c7d8487286663a2
Rubinder Singh <rssandhu2011@gmail.com>
2017-04-30 18:10:40 -0700

    Conf Changed

--- Configuration.py
+++ Configuration.py
@@ -23,37 +23,55 @@ class Service:
         self.internal_port = int(service.find("internal_port").text)
         self.external_port = int(service.find("external_port").text)
         self.start_cmd = service.find("start_cmd").text
-        self.expected_args = {}
+        self.pos_expected_args = {}
+        self.named_expected_args = {}
         args = service.find("expected_args")
         self.max_args = 0
         self.min_args = 0
         self.required_args = {}
         self.optional_args = {}
-        i = 0
+
         for arg in args:
             self.max_args += 1
             if arg.attrib.get('required', 0) == "1":
                 self.min_args += 1
                 if arg.attrib.get('name', None) is not None:
-                    self.expected_args[arg.attrib['name']] = arg
-                    self.required_args[arg.attrib['name']] = arg
-                elif arg.attrib.get('position', None) is not None:
-                    self.expected_args[int(arg.attrib['position'])] = arg
-                    self.required_args[int(arg.attrib['position'])] = arg
-                else:
-                    self.expected_args[i] = arg
-                    self.required_args[i] = arg
+                    if not self.named_expected_args.has_key(arg.attrib['name']):
+                        self.named_expected_args[arg.attrib['name']] = arg
+                        self.required_args[arg.attrib['name']] = arg
+                    else:
+                        print "Duplicate name argument"
+                        exit(0)
+                elif arg.attrib.get('position', None) is None:
+                    print "Position is required if name not given"
+                    exit(0)
+
+                if arg.attrib.get('position', None) is not None:
+                    if not self.pos_expected_args.has_key(int(arg.attrib['position'])):
+                        self.pos_expected_args[int(arg.attrib['position'])] = arg
+                        self.required_args[int(arg.attrib['position'])] = arg
+                    else:
+                        print "Duplicate position argument"
+                        exit(0)
             else:
                 if arg.attrib.get('name', None) is not None:
-                    self.expected_args[arg.attrib['name']] = arg
-                    self.optional_args[arg.attrib['name']] = arg
-                elif arg.attrib.get('position', None) is not None:
-                    self.expected_args[int(arg.attrib['position'])] = arg
-                    self.optional_args[int(arg.attrib['position'])] = arg
-                else:
-                    self.expected_args[i] = arg
-                    self.optional_args[i] = arg
-            i = i+1
+                    if not self.named_expected_args.has_key(arg.attrib['name']):
+                        self.named_expected_args[arg.attrib['name']] = arg
+                        self.optional_args[arg.attrib['name']] = arg
+                    else:
+                        print "Duplicate name argument"
+                        exit(0)
+                elif arg.attrib.get('position', None) is None:
+                    print "Position is required if name not given"
+                    exit(0)
+
+                if arg.attrib.get('position', None) is not None:
+                    if not self.pos_expected_args.has_key(int(arg.attrib['position'])):
+                        self.pos_expected_args[int(arg.attrib['position'])] = arg
+                        self.optional_args[int(arg.attrib['position'])] = arg
+                    else:
+                        print "Duplicate position argument"
+                        exit(0)
 
         filterXML = service.find("filters")
         self.filters = {}
